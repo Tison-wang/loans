5c733eb36aad824adf0317ad27789374ff2ff7be
tiansheng.wang <wtsheng88@163.com>
2020-04-03 09:16:35 +0800

    add eureka client annotation

--- src/main/java/com/loan/LoansApplication.java
+++ src/main/java/com/loan/LoansApplication.java
@@ -2,8 +2,10 @@ package com.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableTransactionManagement // 开启事务
 public class LoansApplication {
